0ffb8e34523f7d7d822dd2c28539404fa2bcc1ac
jiamin.dou <jiamin.dou@aispeech.com>
2018-02-14 21:59:32 +0800

    添加 转化为dataframe

--- build.sbt
+++ build.sbt
@@ -6,7 +6,10 @@ scalaVersion := "2.10.5"
 //scalaVersion := "2.11.0"
 
 libraryDependencies ++= Seq (
+  "org.apache.spark" %% "spark-core" % "1.6.3",
   "org.apache.spark" %% "spark-sql" % "1.6.3",
+  "org.apache.spark" %% "spark-hive" % "1.6.3",
+  "org.apache.spark" %% "spark-hive-thriftserver" % "1.6.3",
   "com.alibaba" % "fastjson" % "1.2.12",
   "org.scalatest" %% "scalatest" % "3.0.1" % "test"
 )
--- src/main/scala/com/aispeech/hw.scala
+++ src/main/scala/com/aispeech/hw.scala
@@ -1,23 +1,20 @@
 package com.aispeech
 
 import com.alibaba.fastjson.JSON
+import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.sql.types.{IntegerType, StructField, StructType}
+import org.apache.spark.sql.{Row, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.Row
 
 /**
   * Created by tianyi on 14/02/2018.
   */
 object Hw {
 
-  var schemaArr = List(
+  case class Person(name:String,age:Int)
+  var schemaArr:List[(String, Int)] = List(
     ("eventName", 0),
-    ("sessionId", 0),
-    ("recordId", 0),
-    ("contextId", 0),
-    ("logTime", 0),
-    ("message_authorizer_access_token", 1),
-    ("message_method", 0),
-    ("message_output_status", 0)
+    ("sessionId", 0)
   )
 
   def main(args: Array[String]) {
@@ -25,12 +22,34 @@ object Hw {
     conf.setAppName("check first app")
     conf.setMaster("local")
     val sc = new SparkContext(conf)
-    val rdd = sc.textFile("/data2/user_data/his/data/djm/scala/data/json.txt")
+    val hc = new HiveContext(sc)
+    val sqlContext = new SQLContext(sc)
+    //val rdd = sc.textFile("/data2/user_data/his/data/djm/scala/data/json.txt")
     //val rdd = sc.textFile("/Users/tianyi/speech/testScala/data/json.txt")
-    println("count is ", rdd.count())
-    println("Hi!")
-    System.out.println("testing")
-    Row()
+    val df = hc.sql("select json from his.ba_online_prod  limit 10")
+    //val df = sqlContext.sql("select json from his.ba_online_prod  limit 10")
+    val resDf = df.map(ele => {
+      try {
+        parseJsonMain(ele(1).toString)
+        Row(ele(0).toString.size)
+      } catch {
+        case e => {
+          println(e)
+          Row(ele(0).toString.size)
+        }
+      }
+    })
+    val structType = StructType(
+      Array(
+        StructField("length", IntegerType, true)
+      )
+    )
+    //val structType = StructType(schemaArr.map(fieldName => StructField(fieldName._1, StringType)))
+    //var df = sqlContext.createDataFrame(resDf, structType)
+    println("length : ", resDf.filter(f => f.length > 0).count())
+    val resAllData = sqlContext.createDataFrame(resDf.filter(f => f.length > 0), structType)
+    println("count is ", df.count())
+    resAllData.show
   }
 
   def parseJsonMain(json: String): Row = {
@@ -47,6 +66,7 @@ object Hw {
       if (prefix != "") {
         schemaName = prefix + "__" + schemaName
       }
+      println(schemaName)
       val dataType = dataValue.getClass.getSimpleName
       if (dataType == "String" || dataType == "Integer") {
         val schemaIdx = schemaArr.map(r => r._1).indexOf(schemaName)
